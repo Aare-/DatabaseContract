fb16b556ac1a3651957f18bab53bbe4aaad374a4
Filip Loster <filip.loster@pragmaticcoders.com>
2018-02-08 13:06:21 +0100

    Refactored tests

--- test/database.test.ts
+++ test/database.test.ts
@@ -17,6 +17,67 @@ contract('DatabaseBase', accounts => {
   const user2 = accounts[2];
   const user3 = accounts[3];
 
+  enum OperationType {
+    Registration,
+    DeRegistration,
+    Invalid
+  }
+
+  /*
+   Performs operations on dContract using following scheme:
+   <d/r><1/2/3>
+   where
+   r - registers account
+   d - de-registers account
+   1,2,3 - account number
+   */
+  async function performOperationsOnContract(operations: string) {
+    operations = operations.replace(/ /g, '');
+    const operationsCount = Math.floor(operations.length / 2);
+
+    for (let i = 0; i < operationsCount; i++) {
+      const operationType: OperationType = getOperationType(operations[i * 2]);
+      const account = getOperationAccount(operations[i * 2 + 1]);
+
+      if (operationType === OperationType.Invalid || account === ZERO_ADDRESS) {
+        throw new Error('Error in provided operations list: ' + operations);
+      }
+
+      switch (operationType) {
+        case OperationType.Registration:
+          await dContract.registerAddress(account);
+          break;
+        case OperationType.DeRegistration:
+          await dContract.deRegisterAddress(account);
+          break;
+      }
+    }
+  }
+
+  function getOperationType(operationSymbol: string): OperationType {
+    switch (operationSymbol) {
+      case 'r':
+        return OperationType.Registration;
+      case 'd':
+        return OperationType.DeRegistration;
+      default:
+        return OperationType.Invalid;
+    }
+  }
+
+  function getOperationAccount(accountSymbol: string) {
+    switch (accountSymbol) {
+      case '1':
+        return user1;
+      case '2':
+        return user2;
+      case '3':
+        return user3;
+      default:
+        return ZERO_ADDRESS;
+    }
+  }
+
   beforeEach(async () => {
     dContract = await DatabaseContract.new();
   });
@@ -29,14 +90,14 @@ contract('DatabaseBase', accounts => {
 
   describe('#addressAddition', () => {
     it('should allow to add addresses', async () => {
-      await dContract.registerAddress(user1);
+      await performOperationsOnContract('r1');
     });
 
     it('should disallow adding duplicate addresses', async () => {
-      await dContract.registerAddress(user1);
+      await performOperationsOnContract('r1');
 
       await assertReverts(async () => {
-        await dContract.registerAddress(user1);
+        await performOperationsOnContract('r1');
       });
     });
 
@@ -55,20 +116,19 @@ contract('DatabaseBase', accounts => {
 
   describe('#addressDeletion', () => {
     it('should allow to delete an address', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.deRegisterAddress(user1);
+      await performOperationsOnContract('r1 d1');
       assert.isFalse(await dContract.isAddressRegistered(user1));
     });
 
     it('should revert when de-registering not registered address', async () => {
       await assertReverts(async () => {
-        await dContract.deRegisterAddress(user1);
+        await performOperationsOnContract('d1');
       });
     });
 
     it('should allow only owner for deleting addresses', async () => {
       await assertReverts(async () => {
-        await dContract.registerAddress(user1);
+        await performOperationsOnContract('r1');
         await dContract.deRegisterAddress(user1, { from: user2 });
       });
     });
@@ -107,19 +167,14 @@ contract('DatabaseBase', accounts => {
     });
 
     it('should return multiple registered addresses', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user3);
+      await performOperationsOnContract('r1 r2 r3');
       const addressesList = await dContract.getAllAddresses();
 
       assert.deepEqual(addressesList, [user3, user2, user1]);
     });
 
     it('should return addresses after deletion of the first', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user3);
-      await dContract.deRegisterAddress(user1);
+      await performOperationsOnContract('r1 r2 r3 d1');
 
       const addressesList = await dContract.getAllAddresses();
 
@@ -127,10 +182,7 @@ contract('DatabaseBase', accounts => {
     });
 
     it('should return addresses after deletion of the last', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user3);
-      await dContract.deRegisterAddress(user3);
+      await performOperationsOnContract('r1 r2 r3 d3');
 
       const addressesList = await dContract.getAllAddresses();
 
@@ -138,10 +190,7 @@ contract('DatabaseBase', accounts => {
     });
 
     it('should return addresses after deletion of the middle', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user3);
-      await dContract.deRegisterAddress(user2);
+      await performOperationsOnContract('r1 r2 r3 d2');
 
       const addressesList = await dContract.getAllAddresses();
 
@@ -149,17 +198,7 @@ contract('DatabaseBase', accounts => {
     });
 
     it('should return addresses after multiple operations', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user3);
-      await dContract.deRegisterAddress(user1);
-      await dContract.registerAddress(user1);
-      await dContract.deRegisterAddress(user2);
-      await dContract.deRegisterAddress(user3);
-      await dContract.registerAddress(user3);
-      await dContract.deRegisterAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user1);
+      await performOperationsOnContract('r1 r2 r3 d1 r1 d2 d3 r3 d1 r2 r1');
 
       const addressesList = await dContract.getAllAddresses();
 
@@ -169,8 +208,7 @@ contract('DatabaseBase', accounts => {
 
   describe('#batchAddressDeletion', () => {
     it('should delete all registered addresses', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
+      await performOperationsOnContract('r1 r2');
       await dContract.deRegisterAll();
 
       assert.isFalse(await dContract.isAddressRegistered(user1));
@@ -178,8 +216,7 @@ contract('DatabaseBase', accounts => {
     });
 
     it('should not list any addresses after deletion', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
+      await performOperationsOnContract('r1 r2');
       await dContract.deRegisterAll();
       const addressesList = await dContract.getAllAddresses();
 
@@ -187,11 +224,10 @@ contract('DatabaseBase', accounts => {
     });
 
     it('single address deletion should cost less than 10000 gas', async () => {
-      await dContract.registerAddress(user1);
+      await performOperationsOnContract('r1');
       const transactionReceipt1 = await dContract.deRegisterAll();
 
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
+      await performOperationsOnContract('r1 r2');
       const transactionReceipt2 = await dContract.deRegisterAll();
 
       const gasUsageDelta =
@@ -210,24 +246,21 @@ contract('DatabaseBase', accounts => {
 
   describe('#getNextAddress', () => {
     it('should correctly return address for given predecessor', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
+      await performOperationsOnContract('r1 r2');
 
       const nextAddress = await dContract.getNextAddress(user2);
       assert.deepEqual(nextAddress, user1);
     });
 
     it('should return address when asked for the last address', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user3);
+      await performOperationsOnContract('r1 r2 r3');
 
       const nextAddress = await dContract.getNextAddress(user1);
       assert.deepEqual(nextAddress, user1);
     });
 
     it('should return address(0) for not registered addresses', async () => {
-      await dContract.registerAddress(user1);
+      await performOperationsOnContract('r1');
 
       const nextAddress = await dContract.getNextAddress(user2);
       assert.deepEqual(nextAddress, ZERO_ADDRESS);
@@ -242,19 +275,14 @@ contract('DatabaseBase', accounts => {
     });
 
     it('should correctly report address count for populated list', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user3);
+      await performOperationsOnContract('r1 r2 r3');
       const addressCount = await dContract.countAddresses();
 
       assertNumberEqual(addressCount, new BigNumber(3));
     });
 
     it('should correctly report size after address changes', async () => {
-      await dContract.registerAddress(user1);
-      await dContract.deRegisterAddress(user1);
-      await dContract.registerAddress(user2);
-      await dContract.registerAddress(user3);
+      await performOperationsOnContract('r1 d1 r2 r3');
 
       const addressCount = await dContract.countAddresses();
 
